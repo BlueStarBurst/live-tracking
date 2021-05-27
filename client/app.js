b609618a05706551280c72f52aa79f48bb49318e
BlueStarBurst <hargreaves.bryant@gmail.com>
2021-05-27 15:31:25 -0500

    init server and react

--- client/app.js
+++ client/app.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { render } from 'react-dom'
 
 import * as tf from "@tensorflow/tfjs";
@@ -15,13 +15,46 @@ async function estimatePoseOnImage(imageElement) {
     return pose;
 }
 
-const imageElement = document.getElementById('cat');
+//const imageElement = document.getElementById('cat');
 
-const pose = estimatePoseOnImage(imageElement);
+//const pose = estimatePoseOnImage(imageElement);
 
-console.log(pose);
+//console.log(pose);
 
-render(
-    <>
-        <h1> Hello </h1>
-    </>, document.getElementById("root"));
+var stream = ''
+
+async function Media() {
+    navigator.mediaDevices.getUserMedia({ audio: true, video: true })
+        .then(function (str) {
+            stream = str;
+        })
+        .catch(function (err) {
+            console.log(err)
+        });
+}
+
+function App(props) {
+
+    const [state, setstate] = useState(<Loading />)
+
+    useEffect(() => {
+        Media().then(
+            setstate(<h1> Hello </h1>)
+        )
+    }, [])
+
+    return (state);
+}
+
+function Loading(props) {
+    return <div style={{ backgroundColor: 'black', width: "100vw", height: "100vh" }}></div>
+}
+
+function Model(props) {
+    
+}
+
+
+
+
+render(<App stream={stream} />, document.getElementById("root"));
--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 const express = require('express');
+var fs = require('fs');
+var http = require('http');
+var https = require('https');
 const app = new express();
 
 const { networkInterfaces } = require('os');
@@ -10,27 +13,60 @@ const results = Object.create(null); // Or just '{}', an empty object
 
 for (const name of Object.keys(nets)) {
   for (const net of nets[name]) {
-      // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
-      if (net.family === 'IPv4' && !net.internal) {
-          if (!results[name]) {
-              results[name] = [];
-          }
-          results[name].push(net.address);
-          console.log(net.address);
+    // Skip over non-IPv4 and internal (i.e. 127.0.0.1) addresses
+    if (net.family === 'IPv4' && !net.internal) {
+      if (!results[name]) {
+        results[name] = [];
       }
+      results[name].push(net.address);
+      console.log(net.address);
+    }
   }
 }
 
 console.log(results);
 
-var location = 'docs';
+// var location = 'docs';
 
-app.get('/', function(request, response){
-    response.sendFile(__dirname + '/' + location + '/index.html');
+// app.get('/', function (request, response) {
+//   console.log("GET")
+//   response.sendFile(__dirname + '/' + location + '/index.html');
+// });
+
+// app.use(express.static(location))
+
+const serverConfig = {
+  key: fs.readFileSync(__dirname + '/server/key.pem'),
+  cert: fs.readFileSync(__dirname + '/server/cert.pem'),
+};
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/docs/index.html');
 });
 
-app.use(express.static(location))
+app.use(express.static('docs'))
 
-app.listen(3000, () => {
-  console.log("server running!");
+const httpsServer = https.createServer(serverConfig, app);
+//httpsServer.listen(HTTPS_PORT, '0.0.0.0');
+
+httpsServer.listen(443, () => {
+  console.log("running!");
+});
+
+// app.listen(3000, () => {
+//   console.log("server running!");
+// })
+
+// set up plain http server
+var http = express();
+
+// set up a route to redirect http to https
+http.get('*', function(req, res) {  
+    res.redirect('https://' + req.headers.host + req.url);
+
+    // Or, if you don't want to automatically detect the domain name from the request header, you can hard code it:
+    // res.redirect('https://example.com' + req.url);
 })
+
+// have it listen on 8080
+http.listen(80);
